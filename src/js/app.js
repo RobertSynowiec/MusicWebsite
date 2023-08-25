5469ba85677038c242255bdbe7d2934d59df3788
Robert Synowiec <rsynowiec81@gmail.com>
2023-08-25 20:15:20 +0200

    Add method AppInitData

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,30 @@
 import { settings, templates, classNames, } from '../js/settings.js';
 import SongList from '../js/components/SongList.js';
+import { dataSource } from '../js/data.js';
+
 const app = {
 
-  initSongPlayer: () => {
+  initSongPlayer: function () {  //method
 
-    const player = new SongList();
-    console.log('player ', player);
+    const song = new SongList();
+    console.log('player ', song);
+    console.log('this.data:', this.data);
+  },
+  initData: function () {
+
+    this.data = dataSource; // preparing access to data from the dataSource object - reference
   },
 
   init: function () {
-    const thisApp = this;
+
     console.log('*** App starting ***');
-    console.log('thisApp:', thisApp);
+    console.log('this:', this);
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    this.initData();
+    this.initSongPlayer();
   },
 };
 app.init();
