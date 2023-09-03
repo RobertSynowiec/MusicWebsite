bc8ea7105ac814e0283526f0dc99dd904d460223
Robert Synowiec <rsynowiec81@gmail.com>
2023-09-03 17:43:17 +0200

    add @media 576px

--- src/js/settings.js
+++ src/js/settings.js
@@ -29,8 +29,8 @@ export const classNames = {
 };
 export const settings = {
   db: {
-    url: '//localhost:3131',
-    //url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
+    //url: '//localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname == 'localhost' ? ':3131' : ''),
     songs: 'songs',
   }
 };
